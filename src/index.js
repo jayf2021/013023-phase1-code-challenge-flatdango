24bd74622a8bbb289d59005ea1c10b92fadaeda3
James Fields <fieldsjay13@gmail.com>
2023-02-09 11:28:14 -0700

    submission for deadline

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 // Your code here
+const baseURL = "http://localhost:3000/films";
+const listHolder = document.getElementById('films')
+
+COMPLETELY LOST 
+EVERYTHING I FEEL LIKE I KNOW HAS LEFT MY BRAIN 
+GOING TO BE A VERY BUSY WEEKEND
